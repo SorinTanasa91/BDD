1d589f80d8748eb2c3eded2328066afc40530fb5
Sorin <sd.tanasa@gmail.com>
2023-02-01 14:38:44 +0200

    new

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,13 +1,15 @@
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common import by
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
 from browser import Browser
-
 
 class LoginPage(Browser):
     USERNAME_INPUT=(By.ID,"user-name")
     PASSWORD_INPUT=(By.ID,"password")
     LOGIN_BUTTON=(By.ID,'login-button')
+    ERROR_MESSAGE =(By.CLASS_NAME,'error-button')
 
     def __init__(self,login_button):
         self.login_button = login_button
@@ -17,3 +19,18 @@ class LoginPage(Browser):
 
     def set_user_name(self,user_name):
         self.driver.find_element(*self.USERNAME_INPUT).send_keys(user_name)
+
+    def set_password(self,password):
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
+
+    def click_login_button(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+
+    def error_message(self,expected_message):
+        try:
+            actual_message = self.driver.find_element(*self.ERROR_MESSAGE).text
+        except NoSuchElementException:
+            actual_message = 'None' #Elementul nu exista pe pagina
+
+        assert expected_message == actual_message, f'Error, message is incorect, expected {expected_message}, actual {actual_message}'
+
---
+++ steps/login_steps.py
@@ -0,0 +1,26 @@
+from behave import *
+
+
+@given('login: I am not an user on the login page')
+def step_impl(context):
+    context.login_page.go_to_login_page()
+
+
+@when('login: I fill the username with a wrong value "{user}"')
+def step_impl(context, user):
+    context.login_page.set_user_name(user)
+
+
+@when('login: I fill the password with a wrong value "pwd"')
+def step_impl(context, pwd):
+    context.login_page.set_password(pwd)
+
+
+@when('login: I click the login button')
+def step_impl(context):
+    context.login_page.click_login_button()
+
+
+@then('login: Error message is display with the message "{error_message}"')
+def step_impl(context, error_message):
+    context.login_page.display_error_message(error_message)
